d1f73a487d9c1d0a217659727a0043bab67e43d8
Santosh Kumar <santoshkumar964887@gmail.com>
2020-07-04 12:06:49 +0530

    Multiple Pointers - averagePairsorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the average target

---
+++ Multiple Pointers - averagePair/averageofPairOfArray.js
@@ -0,0 +1,37 @@
+/*Multiple Pointers - averagePair
+Write a function called averagePair. Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the average target.
+
+Bonus Constraints:
+
+Time: O(N)
+
+Space: O(1)
+
+Sample Input:
+
+averagePair([1,2,3],2.5) // true
+averagePair([1,3,3,5,6,7,10,12,19],8) // true
+averagePair([-1,0,3,4,5,6], 4.1) // false
+averagePair([],4) // false
+*/
+function averagePair(arr,avg){
+    var len=arr.length;
+    var leftPointer=0,rightPointer=len-1;
+    while(leftPointer<=rightPointer){
+    var avgSum=((arr[leftPointer]+arr[rightPointer])/2).toFixed(2);
+    
+    if(avg==avgSum)return true;
+    else if(avg>avgSum)leftPointer++;
+    else if(avg<avgSum)rightPointer--; 
+    
+    
+    
+    }
+    return false;
+    }
+    
+    console.log(averagePair([1,2,3],2.5));
+    console.log(averagePair([1,3,3,5,6,7,10,12,19],8));
+    console.log(averagePair([-1,0,3,4,5,6], 4.1));
+     
+    
